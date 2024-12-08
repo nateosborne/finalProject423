f1d4452ea553fd9da2552ef31376ff2677fae70b
nateosborne <natedresdeno@gmail.com>
2024-12-08 01:38:05 -0500

    home

---
+++ src/firebaseConfig.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBGnsg54nMzBt3130FqrxaXpPfGZ56NVcw",
+  authDomain: "n322-no.firebaseapp.com",
+  projectId: "n322-no",
+  storageBucket: "n322-no.appspot.com",
+  messagingSenderId: "206597060127",
+  appId: "1:206597060127:web:e5ef15d1b88f90c33b872a",
+  measurementId: "G-WZSTDBWX6H",
+};
--- src/index.js
+++ src/index.js
@@ -1,7 +1,53 @@
 import * as $ from "jquery";
 
 function initListeners() {
-  console.log("hello");
+    console.log("hello");
+    getInfo();
+}
+
+export function getInfo() {
+  var albumURL =
+    [
+    "https://musicbrainz.org/ws/2/release-group/299a1b9f-7cdd-46da-a5ea-77571ce04009?inc=aliases%2Bartist-credits%2Breleases&fmt=json",
+    "https://musicbrainz.org/ws/2/release-group/1b022e01-4da6-387b-8658-8678046e4cef?inc=aliases%2Bartist-credits%2Breleases&fmt=json",
+    "https://musicbrainz.org/ws/2/release/9651aac2-4296-381f-a595-f11e3242c997?inc=aliases%2Bartist-credits%2Blabels%2Bdiscids%2Brecordings&fmt=json",
+    "https://musicbrainz.org/ws/2/release-group/aa997ea0-2936-40bd-884d-3af8a0e064dc?inc=aliases%2Bartist-credits%2Breleases&fmt=json"
+    ];
+
+
+
+    $.each(albumURL, function(index, val){
+        // console.log(index, val)
+    
+
+  $.getJSON(albumURL[index], (data) => {
+    // console.log(data);
+    // console.log(`${data['artist-credit']['name']}`);
+
+    $(".row").append(`
+            <div class="album">
+                <div class="cover">
+                    <img src="${data.title}" width="250px" height="100%">
+                </div>
+                <div class="album-info">
+                    <div class="title">
+                        <p>${data.title}</p>
+                        <p class="orange">/10</p>
+                    </div>
+                    <div class="artist">
+                        <p>Artist</p>
+                    </div>
+                    <div class="title">
+                        <p>Genre</p>
+                        <p class="orange">${data["first-release-date"]}</p>
+                    </div>
+                </div>
+            </div>
+        `);
+  }).fail((error) => {
+    console.log("error", error.message);
+  });
+})
 }
 
 $(document).ready(function () {
